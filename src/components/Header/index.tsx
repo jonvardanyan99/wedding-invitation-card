b5dc68361e4d66fd422fc0c59ce6abf7cc55ca65
jonvardanyan99 <john.vardanyan99@gmail.com>
2026-04-30 22:20:00 +0400

    Add arrow down animation

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,7 +8,7 @@ export const Header: React.FC = () => {
     <header className="relative w-full h-dvh flex flex-col justify-center items-center z-3">
       <h1 className="text-[100px] font-bold italic text-white text-shadow-1 animate-text-appear [animation-duration:4s] max-[970px]:text-[80px] max-[770px]:text-[60px] max-[580px]:text-[40px] max-[400px]:text-[30px]">Ֆելիքս և Ամալյա</h1>
       <p className="text-[70px] font-medium text-white text-shadow-1 animate-text-appear [animation-duration:4.5s] max-[970px]:text-[50px] max-[770px]:text-[40px] max-[580px]:text-[25px] max-[400px]:text-[20px]">21 Հունիս 2026</p>
-      <a href={`#${WEDDING_DATE_SECTION_ID}`} className="absolute bottom-7.5 size-17.5 flex justify-center items-center rounded-full transition-all hover:bg-black/30">
+      <a href={`#${WEDDING_DATE_SECTION_ID}`} className="absolute bottom-7.5 size-17.5 flex justify-center items-center rounded-full transition-all hover:bg-black/30 animate-arrow-move">
         <img src={arrowDownIcon} alt="Arrow down" className="w-17.5 h-10 object-contain mt-1.25" />
       </a>
     </header>
--- src/components/WeddingDetails/index.tsx
+++ src/components/WeddingDetails/index.tsx
@@ -13,7 +13,7 @@ export const WeddingDetails: React.FC = () => {
       <div className="relative z-3 p-[80px_30px] max-[500px]:p-[60px_20px]">
         <div id={WEDDING_DATE_SECTION_ID} className="flex flex-col items-center">
           <h2 className="max-w-100 text-[42px] font-medium italic text-white text-shadow-1 animate-text-appear [animation-duration:4s] mb-22 max-[970px]:text-[32px] max-[970px]:max-w-75 max-[770px]:text-[28px] max-[770px]:max-w-65 max-[500px]:mb-17 max-[400px]:text-[22px] max-[400px]:max-w-51.25">Սիրելի՛ ընկերներ և հարազատներ</h2>
-          <a href={`#${WEDDING_DATE_SECTION_ID}`} className="size-17.5 flex justify-center items-center rounded-full transition-all mb-22 hover:bg-black/30 max-[500px]:mb-17">
+          <a className="size-17.5 flex justify-center items-center rounded-full transition-all mb-22 hover:bg-black/30 animate-arrow-move max-[500px]:mb-17">
             <img src={arrowDownIcon} alt="Arrow down" className="w-17.5 h-10 object-contain mt-1.25" />
           </a>
           <Calendar selected={weddingDate} month={weddingDate} className="mb-22 max-[500px]:mb-17" />
